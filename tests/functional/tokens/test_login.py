d90f3e7c057237e5f6d6f09c340fd7dbc202d760
Kairo de Araujo <kdearaujo@vmware.com>
2022-11-21 13:03:38 +0100

    Fix lint for BDD/FT tests
    - Fixed lints and pep8 for test_login.py
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/functional/tokens/test_login.py
+++ tests/functional/tokens/test_login.py
@@ -6,10 +6,11 @@ from pytest_bdd.parsers import parse
 def test_login_using_rstuf_api():
     """Login using RSTUF API."""
 
+
 @given(
     parse(
-        "the API requester prepares 'data' with {username}, {password}, {scope},"
-        " {expires}"
+        "the API requester prepares 'data' with {username}, {password},"
+        "{scope}, {expires}"
     ),
     target_fixture="data",
 )
@@ -27,6 +28,7 @@ def the_api_requester_prepares_data(username, password, scope, expires):
         "expires": expires,
     }
 
+
 @when(
     "the API requester sends a 'POST' method to '/api/v1/token' with 'data'",
     target_fixture="response",
@@ -34,9 +36,11 @@ def the_api_requester_prepares_data(username, password, scope, expires):
 def the_api_requester_sends_data(data, http_request):
     return http_request(method="POST", url="/api/v1/token", data=data)
 
+
 @then("the API requester should get status code '200'")
 def the_api_requester_should_get_status_code_200(response):
     assert response.status_code == 200
+
 
 @then("the API requester should get 'access_token' in response body")
 def the_api_requester_should_get_access_token_in_response_body(response):
@@ -50,16 +54,20 @@ def the_api_requester_should_get_access_token_in_response_body(response):
 def test_login_using_rstuf_api_gets_unauthorized():
     """Login using RSTUF API gets Unauthorized."""
 
+
 @when(
-    "the API requester sends a 'POST' method to '/api/v1/token' with invalid username/password in 'data'",
+    "the API requester sends a 'POST' method to '/api/v1/token' with invalid "
+    "username/password in 'data'",
     target_fixture="unauthorized_response",
 )
-def the_api_requester_sends_data(data, http_request):
+def the_api_requester_sends_data_unauthorized(data, http_request):
     return http_request(method="POST", url="/api/v1/token", data=data)
+
 
 @then("the API requester should get status code '401'")
 def the_api_requester_should_get_status_code_401(unauthorized_response):
     assert unauthorized_response.status_code == 401
+
 
 @then(
     "the API requester should get 'detail: Unauthorized' in the response body"
@@ -77,16 +85,20 @@ def the_api_requester_should_get_detail_unauthorized_in_the_response_body(
 def test_login_using_rstuf_api_gets_forbidden_for_invalid_scopes():
     """Login using RSTUF API gets forbidden for invalid scopes."""
 
+
 @when(
-    "the API requester sends a 'POST' method to '/api/v1/token' with invalid scopes in 'data'",
+    "the API requester sends a 'POST' method to '/api/v1/token' with invalid "
+    "scopes in 'data'",
     target_fixture="invalid_scope_response",
 )
 def the_api_requester_sends_invalid_scopes_in_data(data, http_request):
     return http_request(method="POST", url="/api/v1/token", data=data)
 
+
 @then("the API requester should get status code '403'")
 def the_api_requester_should_get_status_code_403(invalid_scope_response):
     assert invalid_scope_response.status_code == 403
+
 
 @then("the API requester should get 'scope invalid' in the response body")
 def the_api_requester_should_get_scope_invalid_in_the_response_body(
@@ -105,12 +117,20 @@ def the_user_has_the_repositoryservicetuf_rstuf_is_installed(rstuf_cli):
     rc, _ = rstuf_cli("-h")
     assert rc == 0
 
+
 @given(
-    parse("the user types the command 'rstuf admin login -f -s http://localhost -u {username} -p {password} -e {expires}'"),
-    target_fixture="command"
+    parse(
+        "the user types the command 'rstuf admin login -f -s http://localhost "
+        "-u {username} -p {password} -e {expires}'"
+    ),
+    target_fixture="command",
 )
 def the_user_types_command(username, password, expires):
-    return f"admin login -f -s http://localhost -u {username} -p {password} -e {expires}"
+    return (
+        f"admin login -f -s http://localhost -u {username} -p {password} -e "
+        f"{expires}"
+    )
+
 
 @when("the user 'enter' the login command", target_fixture="command_response")
 def the_user_enter_login_command(rstuf_cli, command):
@@ -120,18 +140,21 @@ def the_user_enter_login_command(rstuf_cli, command):
 @then("the user receives 'Login successful.'")
 def the_user_receives_login_successful(command_response):
     assert command_response[0] == 0
-    assert 'Login successful' in command_response[1]
+    assert "Login successful" in command_response[1]
 
 
-
-@scenario("../../features/tokens/login.feature", "Login using RSTUF CLI gets Unauthorized")
+@scenario(
+    "../../features/tokens/login.feature",
+    "Login using RSTUF CLI gets Unauthorized",
+)
 def test_login_using_rstuf_cli_gets_unauthorized():
     """Login using RSTUF CLI gets Unauthorized."""
 
 
 @when(
-    "the user 'enter' the login command with invalid username/password credentials",
-    target_fixture="command_unauthorized_response"
+    "the user 'enter' the login command with invalid username/password "
+    "credentials",
+    target_fixture="command_unauthorized_response",
 )
 def the_user_enter_the_command_with_invalid_usernamepassword_credentials(
     rstuf_cli, command
@@ -142,4 +165,4 @@ def the_user_enter_the_command_with_invalid_usernamepassword_credentials(
 @then("the user receives 'Unauthorized'")
 def the_user_receives_unauthorized(command_unauthorized_response):
     assert command_unauthorized_response[0] == 1
-    assert 'Unauthorized' in command_unauthorized_response[1]
+    assert "Unauthorized" in command_unauthorized_response[1]
