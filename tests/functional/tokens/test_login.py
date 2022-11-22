6c8f6177e15518b94fcee36ae9dd7c7afdb01581
Kairo de Araujo <kdearaujo@vmware.com>
2022-11-22 11:21:30 +0100

    Revert "Fix lint after adding comment separator"
    This reverts commit fcc75c891bea1588c7cd8001fe427417dfeb45ab.

--- tests/functional/tokens/test_login.py
+++ tests/functional/tokens/test_login.py
@@ -46,9 +46,7 @@ def the_api_requester_should_get_status_code_200(response):
 def the_api_requester_should_get_access_token_in_response_body(response):
     assert response.json()["access_token"] is not None
 
-
 ###############################################################################
-
 
 @scenario(
     "../../features/tokens/login.feature",
@@ -57,7 +55,6 @@ def the_api_requester_should_get_access_token_in_response_body(response):
 def test_login_using_rstuf_api_gets_unauthorized():
     """Login using RSTUF API gets Unauthorized."""
 
-
 @when(
     "the API requester sends a 'POST' method to '/api/v1/token' with invalid "
     "username/password in 'data'",
@@ -65,7 +62,6 @@ def test_login_using_rstuf_api_gets_unauthorized():
 )
 def the_api_requester_sends_data_unauthorized(data, http_request):
     return http_request(method="POST", url="/api/v1/token", data=data)
-
 
 @then("the API requester should get status code '401'")
 def the_api_requester_should_get_status_code_401(unauthorized_response):
@@ -80,9 +76,7 @@ def the_api_requester_should_get_detail_unauthorized_in_the_response_body(
 ):
     assert unauthorized_response.json()["detail"] == "Unauthorized"
 
-
 ###############################################################################
-
 
 @scenario(
     "../../features/tokens/login.feature",
@@ -90,7 +84,6 @@ def the_api_requester_should_get_detail_unauthorized_in_the_response_body(
 )
 def test_login_using_rstuf_api_gets_forbidden_for_invalid_scopes():
     """Login using RSTUF API gets forbidden for invalid scopes."""
-
 
 @when(
     "the API requester sends a 'POST' method to '/api/v1/token' with invalid "
@@ -112,20 +105,16 @@ def the_api_requester_should_get_scope_invalid_in_the_response_body(
 ):
     assert "forbidden" in invalid_scope_response.json()["detail"]["error"]
 
-
 ###############################################################################
-
 
 @scenario("../../features/tokens/login.feature", "Login using RSTUF CLI")
 def test_login_using_rstuf_cli():
     """Login using RSTUF CLI."""
 
-
 @given("the user has the repository-service-tuf (rstuf) is installed")
 def the_user_has_the_repositoryservicetuf_rstuf_is_installed(rstuf_cli):
     rc, _ = rstuf_cli("-h")
     assert rc == 0
-
 
 @given(
     parse(
@@ -140,20 +129,16 @@ def the_user_types_command(username, password, expires):
         f"{expires}"
     )
 
-
 @when("the user 'enter' the login command", target_fixture="command_response")
 def the_user_enter_login_command(rstuf_cli, command):
     return rstuf_cli(command)
-
 
 @then("the user receives 'Login successful.'")
 def the_user_receives_login_successful(command_response):
     assert command_response[0] == 0
     assert "Login successful" in command_response[1]
 
-
 ###############################################################################
-
 
 @scenario(
     "../../features/tokens/login.feature",
@@ -161,7 +146,6 @@ def the_user_receives_login_successful(command_response):
 )
 def test_login_using_rstuf_cli_gets_unauthorized():
     """Login using RSTUF CLI gets Unauthorized."""
-
 
 @when(
     "the user 'enter' the login command with invalid username/password "
@@ -172,7 +156,6 @@ def the_user_enter_the_command_with_invalid_usernamepassword_credentials(
     rstuf_cli, command
 ):
     return rstuf_cli(command)
-
 
 @then("the user receives 'Unauthorized'")
 def the_user_receives_unauthorized(command_unauthorized_response):
