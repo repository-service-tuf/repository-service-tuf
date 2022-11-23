8fa636785fa5f803aac7fe3cc7a69b7e1923db61
Kairo de Araujo <kdearaujo@vmware.com>
2022-11-23 11:40:20 +0100

    Fix linting for the changes
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -14,11 +14,13 @@ def get_admin_pwd():
 @pytest.fixture
 def rstuf_cli():
     def _run_rstuf_cli(params: List):
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
         result = subprocess.run(
             ["rstuf", "-c", "rstuf.ini"] + params.split(), capture_output=True
         )
 
-        return result.returncode, ansi_escape.sub('', result.stdout.decode("utf-8"))
+        return result.returncode, ansi_escape.sub(
+            "", result.stdout.decode("utf-8")
+        )
 
     return _run_rstuf_cli
