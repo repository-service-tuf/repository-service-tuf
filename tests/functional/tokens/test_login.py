ce5e83585a3715dc0423ec68b4e38229b4a12494
Martin Vrachev <mvrachev@vmware.com>
2023-03-23 17:09:23 +0200

    Fix linting error
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- tests/functional/tokens/test_login.py
+++ tests/functional/tokens/test_login.py
@@ -15,7 +15,6 @@ def test_login_using_rstuf_api():
     target_fixture="data",
 )
 def the_api_requester_prepares_data(username, password, scope, expires):
-
     if expires == "None":
         expires = None
     else:
