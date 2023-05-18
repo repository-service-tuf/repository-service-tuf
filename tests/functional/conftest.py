21859ae33e7e16f8cdaaac3b9bdeaf334d5a7d90
Kairo de Araujo <kdearaujo@vmware.com>
2023-05-18 15:12:14 +0200

    Functional tests uses `RSTUF CLI` with `--auth`
    As implemented by CLI
    (https://github.com/repository-service-tuf/repository-service-tuf-cli/pull/289)
    the functional tests require to use the `--auth` parameters to be
    compatible with the existent tests, these tests use a API running the
    built-in authentication.
    
    Note: We are planning changing the FT to be compatible with issue #41
    This commit doesn't aim this change. It fix the current state.
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -22,7 +22,8 @@ def rstuf_cli():
     def _run_rstuf_cli(params: List):
         ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
         result = subprocess.run(
-            ["rstuf", "-c", "rstuf.ini"] + params.split(), capture_output=True
+            ["rstuf", "--auth", "-c", "rstuf.ini"] + params.split(),
+            capture_output=True,
         )
 
         return result.returncode, ansi_escape.sub(
