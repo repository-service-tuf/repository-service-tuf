6b79beb58387e2226a35495086bebb4e3d094d07
Kairo de Araujo <kdearaujo@vmware.com>
2023-06-20 15:43:52 +0200

    Remove token functional tests
    The functional tests for token is not cross-component and it removes
    from the umbrella repository.
    
    The tests are added to the RSTUF API (PR
    https://github.com/repository-service-tuf/repository-service-tuf-api/pull/369)
    
    This is part of https://github.com/repository-service-tuf/repository-service-tuf/issues/41
    
    Closes: #267
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -13,16 +13,11 @@ from tuf.ngclient import Updater, UpdaterConfig
 
 
 @pytest.fixture
-def get_admin_pwd():
-    return os.getenv("ADMIN_SECRET_TESTS")
-
-
-@pytest.fixture
 def rstuf_cli():
     def _run_rstuf_cli(params: List):
         ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
         result = subprocess.run(
-            ["rstuf", "--auth", "-c", "rstuf.ini"] + params.split(),
+            ["rstuf"] + params.split(),
             capture_output=True,
         )
 
@@ -64,19 +59,6 @@ def http_request():
 
 
 @pytest.fixture
-def access_token(http_request, get_admin_pwd):
-    data = {
-        "username": "admin",
-        "password": get_admin_pwd,
-        "scope": "write:token read:tasks write:targets delete:targets",
-        "expires": 1,
-    }
-    response = http_request(method="POST", url="/api/v1/token", data=data)
-
-    return response.json()["access_token"]
-
-
-@pytest.fixture
 def get_target_info():
     def _run_get_target_info(target_path):
         temp_md_dir = tempfile.TemporaryDirectory()
@@ -98,7 +80,7 @@ def get_target_info():
 @pytest.fixture
 def task_completed_within_threshold():
     def _run_task_completion_check(
-        http_request, headers, response_json, threshold
+        http_request, response_json, threshold
     ):
         """Validates that a task has finished within a threshold of seconds."""
         task_id = response_json["data"]["task_id"]
@@ -112,7 +94,6 @@ def task_completed_within_threshold():
             response = http_request(
                 method="GET",
                 url=f"/api/v1/task/?task_id={task_id}",
-                headers=headers,
             )
             assert response.status_code == 200, response.text
             task_response_json = response.json()
