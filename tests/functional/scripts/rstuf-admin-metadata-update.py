23d03273d48b6a41d60d49340d51d86ef48f05bf
Kairo de Araujo <kdearaujo@vmware.com>
2023-10-07 08:22:20 +0200

    tests: FT (BDD) metadata update
    Adds interactive run of the metadata update feature and the test
    implementation.
    
    The process works by:
    
    - It creates a thread sending "add artifacts"
    - It waits for some tasks to be sent to the broker and runs the
      "metadata update" request.
    - It checks if the metadata update task finishes and waits to receive
      more tasks and stop the thread "add artifacts"
    - It validates if the new 2.root.json is in the metadata storage
    - It waits for all "add artifacts" tasks to finish
    - It validates if the TUF client finds all artifacts to guarantee the
      consistency
    
    This includes interactive metadata updates using the CLI to create the
    the payload for `metadata update`
    It will allow the future use of the DAS for metadata updates in some
    scenario of FT
    
    Signed-off-by: Kairo de Araujo <kdearaujo@vmware.com>

---
+++ tests/functional/scripts/rstuf-admin-metadata-update.py
@@ -0,0 +1,49 @@
+import json
+import os
+import sys
+from tempfile import mkdtemp
+
+from click.testing import CliRunner
+from repository_service_tuf import Dynaconf, cli
+
+
+def _run(input):
+    folder_name = mkdtemp()
+    setting_file = os.path.join(folder_name, "rstuf.yml")
+    context = {"settings": Dynaconf(), "config": setting_file}
+    runner = CliRunner()
+    output = runner.invoke(
+        cli.admin.metadata.update,
+        "",
+        input="\n".join(input),
+        obj=context,
+        catch_exceptions=False,
+    )
+
+    return output
+
+
+def main():
+    input_dict = json.loads(sys.argv[1])
+    input = [i for i in input_dict.values()]
+
+    print("Using parameters:")
+    print(json.dumps(input_dict, indent=2))
+    output = _run(input)
+
+    print(f"\nExit code: {output.exit_code}")
+    print("\nOutput: ")
+    print(output.stdout)
+    if output.stderr_bytes is not None:
+        print(f"\nError\n: {output.stderr}")
+
+    if output.exception:
+        print(f"Exception: {output.exception}")
+        print(f"Exception Info: {output.exc_info}")
+        output.exc_info
+
+    sys.exit(output.exit_code)
+
+
+if __name__ == "__main__":
+    main()
