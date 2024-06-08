f67cbcc1b77980e79863fbc606bb04496619089c
Martin Vrachev <mvrachev@vmware.com>
2024-06-08 19:51:13 +0300

    Ft Signed tests: make root threshold 2 (#742)
    * Ft Signed tests: make root threshold 2
    
    This is needed as a new requirement:
    "root threshold must be above 1" has been added.
    
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>
    
    * Fix additional line for ft-signed
    
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>
    
    * Make parameters unique for ft-signed params
    
    Duplicates in run-ft-signed paramters for the script
    "rstuf-admin-ceremony.py" are removed.
    For example if we have:
    s = '{
        "Please enter path to encrypted private key": "tests/.../JJ.ecdsa",
        "Please enter password": "hunter2",
        "Please enter signing key index, or press enter to continue": "1",
        "Please enter path to encrypted private key": "tests/.../JH.ed25519",
        "Please enter password": "hunter2"
    }'
    
    and do:
    
    import json
    d = json.loads(s)
    print(d)
    
    the result will be:
    
    {
        "Please enter path to encrypted private key": "tests/.../JJ.ecdsa",
        "Please enter password": "hunter2",
        "Please enter signing key index, or press enter to continue": "1",
    }
    
    meaning the answers for the second root key will be removed as there
    are dict items with the same keys.
    
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>
    
    * Update ft-signed md update part
    
    Add signing key for md update, so it can be fully signed.
    
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>
    
    ---------
    
    Signed-off-by: Martin Vrachev <martin.vrachev@broadcom.com>

--- tests/functional/scripts/run-ft-signed.sh
+++ tests/functional/scripts/run-ft-signed.sh
@@ -19,24 +19,25 @@ fi
 python ${UMBRELLA_PATH}/tests/functional/scripts/rstuf-admin-ceremony.py '{
     "Please enter days until expiry for timestamp role (1)": "",
     "Please enter days until expiry for snapshot role (1)": "",
-    "Please enter days until expiry for targets role (1)": "",
+    "Please enter days until expiry for targets role (365)": "",
     "Please enter days until expiry for bins role (1)": "",
     "Please enter number of delegated hash bins [2/4/8/16/32/64/128/256/512/1024/2048/4096/8192/16384] (256)": "2",
     "Please enter days until expiry for root role (365)": "",
-    "Please enter root threshold": "1",
-    "Please enter days until expiry for root role (365)": "",
+    "Please enter root threshold": "2",
     "(root 1) Please enter path to public key": "tests/files/key_storage/JJ.pub",
     "(root 1) Please enter key name": "JanisJoplin",
-    "Please press 0 to add key, or remove key by entering its index. Press enter to continue": "0",
+    "Please press 0 to add key, or remove key by entering its index": "0",
     "(root 2) Please enter path to public key:": "tests/files/key_storage/JH.pub",
-    "(root 2) Please enter key name": "JoeCocker",
+    "(root 2) Please enter key name": "JimiHendrix",
     "(Finish root keys) Please press 0 to add key, or remove key by entering its index. Press enter to continue": "",
     "(online key) Please enter path to public key": "tests/files/key_storage/0d9d3d4bad91c455bc03921daa95774576b86625ac45570d0cac025b08e65043.pub",
     "(online key) Please enter key name": "online1",
     "Please enter signing key index": "1",
     "(root 1) Please enter path to encrypted private key": "tests/files/key_storage/JJ.ecdsa",
-    "Please enter password": "hunter2",
-    "something": ""
+    "(root 1) Please enter password": "hunter2",
+    "Please enter signing key index, or press enter to continue": "1",
+    "(root 2) Please enter path to encrypted private key": "tests/files/key_storage/JH.ed25519",
+    "(root 2) Please enter password": "hunter2"
 }'
 
 # Bootstrap using legacy with DAS
@@ -50,7 +51,7 @@ wget -P metadata/ ${METADATA_BASE_URL}/1.root.json
 
 # Run metadata update to be used later (during FT)
 python ${UMBRELLA_PATH}/tests/functional/scripts/rstuf-admin-metadata-update.py '{
-  "Root expires on 04/16/25. Do you want to change the expiry date? [y/n]": "",
+  "Root expires on 04/16/25. Do you want to change the expiry date? [y/n] (y)": "",
   "Please enter days until expiry for root role (365)": "",
   "Root signature threshold is 1. Do you want to change the threshold? [y/n] (n)": "",
   "Please press 0 to add key, or remove key by entering its index. Press enter to continue": "",
@@ -58,8 +59,11 @@ python ${UMBRELLA_PATH}/tests/functional/scripts/rstuf-admin-metadata-update.py 
   "Please enter path to public key": "tests/files/key_storage/cb20fa1061dde8e6267e0bef0981766aaadae168e917030f7f26edc7a0bab9c2.pub",
   "Please enter key name": "online2",
   "Please enter signing key index": "1",
-  "(root 1) Please enter path to public key": "tests/files/key_storage/JJ.ecdsa",
-  "Please enter password": "hunter2"
+  "(root 1) Please enter path to encrypted private key": "tests/files/key_storage/JJ.ecdsa",
+  "(root 1) Please enter password": "hunter2",
+  "Please enter signing key index, or press enter to continue": "1",
+  "(root 2) Please enter path to encrypted private key": "tests/files/key_storage/JH.ed25519",
+  "(root 2) Please enter password": "hunter2"
 }'
 
 # Copy files when UMBRELLA_PATH is not the current dir (FT triggered from components)
