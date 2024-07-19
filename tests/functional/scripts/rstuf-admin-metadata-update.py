221fa34f8e287b294962bef496240a7d7ac47d6a
Martin Vrachev <mvrachev@vmware.com>
2024-07-19 13:21:04 +0300

    Ft tests: add server to md_update script (#778)

--- tests/functional/scripts/rstuf-admin-metadata-update.py
+++ tests/functional/scripts/rstuf-admin-metadata-update.py
@@ -11,7 +11,9 @@ from repository_service_tuf import Dynaconf, cli
 def _run(input, selection):
     folder_name = mkdtemp()
     setting_file = os.path.join(folder_name, "rstuf.yml")
-    context = {"settings": Dynaconf(), "config": setting_file}
+    test_settings = Dynaconf()
+    test_settings.SERVER = "http://repository-service-tuf-api"
+    context = {"settings": test_settings, "config": setting_file}
 
     runner = CliRunner()
 
